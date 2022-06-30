d762ebc7e6bbc5dde54d4d6505c256a9e15864ad
jennerdulce <jennerdulce@gmail.com>
2022-06-30 11:34:14 -0700

    Completed Backend Strcture Card and User Stories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cats = [
+    {
+      name: 'Mittens',
+      age: 5,
+      enjoys: 'sunshine and warm spots',
+      image: 'https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+      name: 'Raisins',
+      age: 4,
+      enjoys: 'being queen of the dogs',
+      image: 'https://images.unsplash.com/photo-1533743983669-94fa5c4338ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1092&q=80'
+    },
+    {
+      name: 'Toast',
+      age: 1,
+      enjoys: 'getting all the attention',
+      image: 'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    }
+  ]
+
+# 
+
+cats.each do |cat|
+    Cat.create cat
+end
+
+p Cat.all
+p "Cat's created VIA Seed: OK"
